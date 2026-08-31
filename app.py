448e8b21f5e35e13c1a339effa55d680166282aa
Jontie999 <153198011+Jontie999@users.noreply.github.com>
2026-08-31 08:38:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 from daily_summary import build_html
 
 app = Flask(__name__, static_folder='static')
@@ -7,9 +7,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 def index():
     try:
-        return build_html()
+        html = build_html()
+        return Response(html, mimetype="text/html")
     except Exception as e:
         return f"<p>Error generating page: {e}</p>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
