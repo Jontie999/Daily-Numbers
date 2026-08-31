33637559ef498d6303b945e2624b1b98e2962122
Jontie999 <153198011+Jontie999@users.noreply.github.com>
2026-08-31 08:47:27 +0100

    Refactor index function and Flask app setup

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ app = Flask(__name__, static_folder='static')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/")
-from flask import Flask, Response
-from daily_summary import build_html
-
-app = Flask(__name__, static_folder='static')
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-@app.route("/")
 def index():
     try:
         html = build_html()
