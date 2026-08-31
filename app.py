6d215e734965b7e2e93dcba55d87664647060be7
Jontie999 <153198011+Jontie999@users.noreply.github.com>
2026-08-31 10:44:20 +0100

    Refactor app.py to import functions directly

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, Response, make_response
-from daily_summary import build_html
+from flask import Flask
+from daily_summary import (
+    fetch_weather,
+    fetch_tides,
+    fetch_cruise_ships,
+    fetch_vessel_movements,
+    build_message,
+    build_html
+)
 
-app = Flask(__name__, static_folder='static', static_url_path='/static')
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-@app.route('/static/<path:filename>')
-def staticfiles(filename):
-    return app.send_static_file(filename)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
