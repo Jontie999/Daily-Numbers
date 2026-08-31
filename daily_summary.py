0e52ba674b7de9e3d43b4cdc2008dcd82c10a096
Jontie999 <153198011+Jontie999@users.noreply.github.com>
2026-08-31 08:31:50 +0100

    Change stylesheet version in daily summary HTML
    Updated stylesheet version in HTML output.

--- daily_summary.py
+++ daily_summary.py
@@ -11,7 +11,7 @@ def build_html():
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Daily Numbers</title>
-        <link rel="stylesheet" href="/static/style.css?v=1">
+        <link rel="stylesheet" href="/static/style.css?v=3">
 
     </head>
 
