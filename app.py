4bc91f5721a65670678fe99b9c9020e8c79b52ae
Jontie999 <153198011+Jontie999@users.noreply.github.com>
2026-08-31 08:58:52 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, Response
 from daily_summary import build_html
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/static/<path:filename>')
+def staticfiles(filename):
+    return app.send_static_file(filename)
 
 @app.route("/")
 def index():
